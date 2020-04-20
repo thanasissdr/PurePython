cc2dcabe46a206b8b97b477a020f0437dbce5c5c
Thanos Sdralias <thanasis_sdr@outlook.com>
2020-04-20 19:18:48 +0100

    Fixed memory_profile when a positional argument has a space

---
+++ Debugging/main.py
@@ -0,0 +1,21 @@
+from src.basic import Simple
+
+
+x = Simple(10)
+x_squared = x.squared()
+print(x_squared)
+
+
+print(Simple(20).squared())
+for _ in range(10):
+    print('The weather is good')
+
+print(Simple('a').squared())
+
+try:    
+    print('The weather today'**2)
+except:
+    raise TypeError("Type error")
+
+
+print("Exited with status 1")
---
+++ Debugging/src/basic.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+class Simple:
+
+    def __init__(self, a):
+        self.a = a
+
+
+    def squared(self):
+        return self.a**2
+
+
+    def __str__(self):
+        return self.a
+
+    
