4951c2e053910315ae869230c62dc82aeea34c62
Thanos Sdralias <thanasis_sdr@outlook.com>
2021-05-10 22:16:00 +0100

    Modified NLP

--- NLP/preprocessing/base/main.py
+++ NLP/preprocessing/base/main.py
@@ -1,6 +1,10 @@
+import contractions
+import copy
+
 from preprocessing.base.sentences.convert_case import CaseConverter
 from preprocessing.base.sentences.remove_punctuation import PunctuationRemover
 from preprocessing.base.sentences.replace_token import TokenReplacer
+from preprocessing.base.sentences.remove_space import SpaceRemover
 
 
 class SentenceModifier:
@@ -8,15 +12,21 @@ class SentenceModifier:
         pass
 
     def run(self, sentence):
-        self.sentence = sentence.copy()
+        self.sentence = copy.copy(sentence)
 
-        self.replace_tokens()
-        self.case_converter()
-        self.remove_punctuation()
+        self.replace_token()
+        self.get_contractions()
+        self.case_convert()
+        self.remove_punctuation(keep_space=True)
+        self.remove_space()
+
+        return self.sentence
 
     def replace_token(self):
         token_replacer = TokenReplacer()
-        self.sentence = token_replacer.replace_emails()
+        self.sentence = token_replacer.replace_emails(self.sentence)
+        self.sentence = token_replacer.replace_urls(self.sentence)
+        self.sentence = token_replacer.replace_numbers(self.sentence)
         return self
 
     def case_convert(self):
@@ -24,8 +34,24 @@ class SentenceModifier:
         self.sentence = case_converter.to_lower(self.sentence)
         return self
 
-    def remove_punctuation(self, sentence):
+    def remove_punctuation(self, keep_space):
         punctuation_remover = PunctuationRemover()
-        self.sentence = punctuation_remover.remove_punctuation(self.sentence)
+        self.sentence = punctuation_remover.remove_punctuation(
+            self.sentence, keep_space=keep_space
+        )
         return self
 
+    def remove_space(self):
+        space_remover = SpaceRemover()
+        self.sentence = space_remover.remove_inner_space(self.sentence)
+        return self
+
+    def get_contractions(self):
+        self.sentence = contractions.fix(self.sentence)
+        return self
+
+
+if __name__ == "__main__":
+
+    sentence = "I haven't searched either in WWW.GOOGLE.GR or yahoo.com."
+    print(SentenceModifier().run(sentence))
--- NLP/setup.py
+++ NLP/setup.py
@@ -1,3 +1,7 @@
 from setuptools import find_packages, setup
 
-setup(name="my_nlp_app", version="1.0", packages=find_packages())
+setup(
+    name="my_nlp_app",
+    version="1.0.0",
+    packages=find_packages(include=["preprocessing"]),
+)
