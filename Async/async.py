1d6beeb9992cea20a50bf447ea5a264681d25bbb
Thanos Sdr <thanasis_sdr@outlook.com>
2022-07-13 09:59:51 +0100

    refactor(async): Refactored asyncio example

--- Async/async.py
+++ Async/async.py
@@ -22,30 +22,28 @@ def timing_async(f):
 
 @timing_async
 async def quick_function():
-    async with ClientSession() as _:
-        print("Entered quick function")
-        return 1
+    print("Entered quick function")
+    return 1
 
 
 async def _slow_function():
-    await asyncio.sleep(3)
     print("Entered slow function")
+    time.sleep(5)
     return 10
 
 
 @timing_async
 async def slow_function():
-    async with ClientSession() as _:
-        return await _slow_function()
+    slow_task = asyncio.create_task(_slow_function())
+    return await slow_task
 
 
 @timing_async
 async def main():
     time.sleep(1)
 
-    async with ClientSession() as _:
-        tasks = [quick_function(), slow_function(), quick_function()]
-        return await asyncio.gather(*tasks)
+    tasks = [quick_function(), slow_function(), quick_function()]
+    return await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
