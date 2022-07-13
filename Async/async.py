1bd5e58b1d9daeda4c56d9193a6a9cdad5f5994a
Thanos Sdr <thanasis_sdr@outlook.com>
2022-07-13 17:01:33 +0100

    Added async with ProcessPoolExecutor

--- Async/async.py
+++ Async/async.py
@@ -1,11 +1,15 @@
+"""
+ProcessPoolExecutor will not work in jupyter notebook
+and it will lead into errors
+"""
+
+
 import asyncio
-from multiprocessing.connection import Client
-import requests
 import time
 
 from functools import wraps
+from concurrent.futures import ProcessPoolExecutor
 
-from aiohttp import ClientSession
 
 
 def timing_async(f):
@@ -14,37 +18,54 @@ def timing_async(f):
         start = time.perf_counter()
         val = await f(*args, **kwargs)
         end = time.perf_counter()
-        print(f"Async function {f.__qualname__} finished in {end - start:.2f} seconds.")
+        print(f"Async function {f.__qualname__} finished in {end - start:.2f} seconds")
         return val
 
     return inner
 
 
+def timing(f):
+    @wraps(f)
+    def inner(*args, **kwargs):
+        start = time.perf_counter()
+        val = f(*args, **kwargs)
+        end = time.perf_counter()
+        print(f"Function {f.__qualname__} finished in {end - start:.2f} seconds")
+        return val
+
+    return inner
+
+
+def _simple_count(n=10000000):
+    return [i for i in range(n)]
+
+
 @timing_async
-async def quick_function():
-    print("Entered quick function")
-    return 1
+async def simple_count(n=100000000):
+    executor = ProcessPoolExecutor(max_workers=1)
+    loop = asyncio.get_event_loop()
+    t = await loop.run_in_executor(executor, _simple_count, n)
+    return t
 
 
-async def _slow_function():
-    print("Entered slow function")
-    time.sleep(5)
+@timing_async
+async def sleep_short():
+
+    await asyncio.sleep(0.05)
     return 10
 
 
 @timing_async
-async def slow_function():
-    slow_task = asyncio.create_task(_slow_function())
-    return await slow_task
+async def sleep_long():
+    await asyncio.sleep(1)
+    return 20
 
 
-@timing_async
-async def main():
-    time.sleep(1)
-
-    tasks = [quick_function(), slow_function(), quick_function()]
+async def main(n):
+    tasks = [simple_count(n), sleep_short(), sleep_short()]
     return await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    N = 3000
+    print(asyncio.run(main(N)))
