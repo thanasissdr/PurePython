185e4f2e92fcb298d66980fd3e7762313e293adc
Thanos Sdr <thanasis_sdr@outlook.com>
2023-01-10 02:42:23 +0000

    Deleted complex examples for specific design patterns; deleted recursive; added generators vs list comprehension

--- design-patterns/pass_expensive_object/run.py
+++ design-patterns/pass_expensive_object/run.py
@@ -1,36 +1,14 @@
-from pass_expensive_object.expensive_function import run_expensive_function
-from pass_expensive_object.child import Child, ChildBuilder, Child1, Child2
+import time
+from abc import ABC, abstractmethod
+from typing import Protocol
+
+from utils import timing
 
 
-class BaseRunner:
-    def get_expensive_object(self, seconds=3):
-        self.expensive_object = run_expensive_function(seconds)
-
-    def _build_child(self, child: Child):
-        child_builder = ChildBuilder(child)
-        child_builder.add_expensive_object(self.expensive_object)
-        return child_builder.build()
-
-    def run(self, child: Child):
-        self._build_child(child)
-        return child.run()
-
-
-class Main:
-    def setup(self, seconds=3):
-        self.base_runner = BaseRunner()
-        self.base_runner.get_expensive_object(seconds)
-
-    def run(self, child):
-        return self.base_runner.run(child)
-
-
-if __name__ == "__main__":
-    main = Main()
-    main.setup(seconds=4)
-
-    ch1 = Child1()
-    ch2 = Child2()
-
-    print(main.run(ch1))
-    print(main.run(ch2))
+@timing
+def run_expensive_function(seconds):
+    print(f"Taking {seconds} seconds to run")
+    time.sleep(seconds)
+    output = 100
+    print(f"The output of the expensive function is {output}")
+    return output
